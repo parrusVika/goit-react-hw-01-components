93ce9c998dcf1cb2f543dced5495b7a27b131fb1
Парасюк Виктория <107310178+parrusVika@users.noreply.github.com>
2023-01-02 23:42:12 +0300

    summary

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,31 +1,30 @@
 import PropTypes from "prop-types";
-import App from "./App"
-export default function Profile({ username, tag, location, avatar, stats = followers, views, likes }) {
+export default function Profile({ username, tag, location, avatar, stats: followers, views, likes }) {
     return (
         <div class="profile">
             <div class="description">
                 <img
-                    src={user.avatar}
-                    alt={user.username}
+                    src={Profile.avatar}
+                    alt={Profile.username}
                     class="avatar"
                 />
-                <p class="name">{user.username}</p>
-                <p class="tag">{user.tag}</p>
-                <p class="location">{user.location}</p>
+                <p class="name">{Profile.username}</p>
+                <p class="tag">{Profile.tag}</p>
+                <p class="location">{Profile.location}</p>
             </div>
 
-            <ul class={user.stats}>
+            <ul class="stats">
                 <li>
                     <span class="label">followers</span>
-                    <span class="quantity">{user.stats.followers}</span>
+                    <span class="quantity">{Profile.stats.followers}</span>
                 </li>
                 <li>
                     <span class="label">Views</span>
-                    <span class="quantity">{user.stats.views}</span>
+                    <span class="quantity">{Profile.stats.views}</span>
                 </li>
                 <li>
                     <span class="label">Likes</span>
-                    <span class="quantity">{user.stats.likes}</span>
+                    <span class="quantity">{Profile.stats.likes}</span>
                 </li>
             </ul>
         </div>
