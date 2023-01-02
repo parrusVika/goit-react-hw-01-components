337b7c1628da83b575ec29d698d19d0b95e59df6
Парасюк Виктория <107310178+parrusVika@users.noreply.github.com>
2023-01-02 23:30:07 +0300

    summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,17 @@
 import Profile from "components/profile";
 
 
+const user = {
+  "username": "Jacques Gluke",
+  "tag": "jgluke",
+  "location": "Ocho Rios, Jamaica",
+  "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+  "stats": {
+    "followers": 5603,
+    "views": 4827,
+    "likes": 1308
+  }
+}
 
 export const App = () => {
   return (
@@ -15,9 +26,23 @@ export const App = () => {
       }}
     >
       <Profile
-        user={user.a} />
+        key={user.tag}
+        avatar={user.avatar}
+        user={user.username}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+
+      />
     </div>
   );
 };
-
+// export default function App() {
+//   return (
+//     <div></div>
+//   )
+// }
 
--- src/components/profile.js
+++ src/components/profile.js
@@ -1,4 +1,6 @@
-export default function Profile(username, tag, location, avatar, stats = followers, views, likes) {
+import PropTypes from "prop-types";
+import App from "./App"
+export default function Profile({ username, tag, location, avatar, stats = followers, views, likes }) {
     return (
         <div class="profile">
             <div class="description">
@@ -14,18 +16,26 @@ export default function Profile(username, tag, location, avatar, stats = followe
 
             <ul class={user.stats}>
                 <li>
-                    <span class="label">{user.stats.followers}</span>
-                    <span class="quantity">5603</span>
+                    <span class="label">followers</span>
+                    <span class="quantity">{user.stats.followers}</span>
                 </li>
                 <li>
-                    <span class="label">{user.stats.views}</span>
-                    <span class="quantity">4827</span>
+                    <span class="label">Views</span>
+                    <span class="quantity">{user.stats.views}</span>
                 </li>
                 <li>
-                    <span class="label">{user.stats.likes}</span>
-                    <span class="quantity">1308</span>
+                    <span class="label">Likes</span>
+                    <span class="quantity">{user.stats.likes}</span>
                 </li>
             </ul>
         </div>
     );
+}
+
+Profile.propTypes = {
+    username: PropTypes.string,
+    tag: PropTypes.string,
+    location: PropTypes.string,
+    stats: PropTypes.number
+
 }
